0028de8b8577672a620e2159c27fb9856a5b95d0
alageev <alageev01@gmail.com>
2020-06-24 01:01:51 +0300

    idk

---
+++ bingo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bingo
+//
+//  Created by Алексей Агеев on 26.05.2020.
+//  Copyright © 2020 Alexey Ageev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        BingoSelect()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
